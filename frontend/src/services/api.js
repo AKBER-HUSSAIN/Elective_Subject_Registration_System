d8b0abd5156c77c441451c827743b007b9ac3860
Akber Hussain <036akberhussain@gmail.com>
2025-10-09 01:07:49 +0530

    Frontend re designed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api", // backend URL
+    baseURL: "https://elective-registration-system.onrender.com/", // backend URL
 });
 
 // attach token automatically
