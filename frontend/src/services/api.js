c530433fa7573f714f3474796edcd79bed401de0
Akber Hussain <036akberhussain@gmail.com>
2025-10-09 01:34:50 +0530

    Frontend re designed2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://elective-registration-system-tdeq.onrender.com/", // Render backend URL
+    baseURL: import.meta.env.PROD
+        ? "https://elective-registration-system-tdeq.onrender.com/api"  // Production
+        : "http://localhost:5000/api", // Development
 });
 
 // attach token automatically
