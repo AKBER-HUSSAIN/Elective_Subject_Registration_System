c73b702ec2329eeae252bf6520104386e73eff46
Akber Hussain <036akberhussain@gmail.com>
2025-10-20 01:48:29 +0530

    Resolved SPA problem

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
     baseURL: import.meta.env.PROD
-        ? "https://elective-registration-system-tdeq.onrender.com/api"  // Production
+        ? "https://elective-backend.onrender.com/api"  // Update this with your actual backend URL
         : "http://localhost:5000/api", // Development
 });
 
